c5e3aeaef68274f5fe8f18d426339a0496f18f8a
Ishu Bansal <ishubansal1400@gmail.com>
2024-12-28 11:48:23 +0530

    sync

--- server.js
+++ server.js
@@ -1,28 +1,130 @@
 const express = require('express');
 const fs = require('fs');
-const url = require('url');
+
+const PORT = 5050;
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/students", (req, res) => {
+app.get("/", (req, res) => {
+    console.log(req.method);
     console.log(req.url);
-    const parsedUrl = url.parse(req.url, true);
-    console.log(parsedUrl.query.result);
-    fs.readFile('./data.json', (err, data) => {
+    console.log(req.query);
+    res.json({ message: "Hey Everone!" });
+});
+
+app.get("/students", (req, res) => {
+    console.log(req.method);
+    console.log(req.url);
+    console.log(req.query);
+    fs.readFile('./students.json', function (err, data) {
         if (err) {
             console.error(err);
+            res.status(500).json({ message: "unable to open a file on server" });
             return;
         }
-        res.json(JSON.parse(data));
-    })
+        const dataJson = JSON.parse(data);
+        res.json(dataJson);
+    });
+});
+
+app.get("/students/:id", (req, res) => {
+    console.log(req.method);
+    console.log(req.url);
+    console.log("Fetching:", req.params.id);
+    console.log(req.query);
+    fs.readFile('./students.json', function (err, data) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ message: "unable to open a file on server" });
+            return;
+        }
+        const students = JSON.parse(data);
+        const student = students.find(student => student.id == req.params.id);
+        res.json(student);
+    });
 });
 
 app.post("/students", (req, res) => {
-    console.log(req.url);
+    console.log(req.method);
     console.log(req.body);
-    res.json(req.body);
+    let students;
+    try {
+        students = fs.readFileSync('./students.json');
+        students = JSON.parse(students);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "unable to open a file on server" });
+        return;
+    }
+    const { id, name } = req.body;
+    const newStudent = { id, name };
+    students.push(newStudent);
+    fs.writeFile('./students.json', JSON.stringify(students), function (err) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ message: "unable to open a file while writing on server" });
+            return;
+        }
+        res.status(201).json(newStudent);
+    })
 });
 
-app.listen(5050);
+app.patch("/students/:id", (req, res) => {
+    console.log(req.method);
+    console.log(req.url);
+    console.log("Editing:", req.params.id);
+    console.log(req.body);
+    let students;
+    try {
+        students = fs.readFileSync('./students.json');
+        students = JSON.parse(students);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "unable to open a file on server" });
+        return;
+    }
+    students = students.map(student => {
+        return student.id == req.params.id ? { ...student, ...req.body, id: req.params.id } : student;
+    });
+    fs.writeFile('./students.json', JSON.stringify(students), function (err) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ message: "unable to open a file while writing on server" });
+            return;
+        }
+        res.json({
+            message: "Updated successfully"
+        });
+    })
+});
+
+app.delete("/students/:id", (req, res) => {
+    console.log(req.method);
+    console.log(req.url);
+    console.log("Deleting:", req.params.id);
+    let students;
+    try {
+        students = fs.readFileSync('./students.json');
+        students = JSON.parse(students);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "unable to open a file on server" });
+        return;
+    }
+    students = students.filter(student => student.id != req.params.id);
+    res.json(students);
+    fs.writeFile('./students.json', JSON.stringify(students), function (err) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ message: "unable to open a file while writing on server" });
+            return;
+        }
+        res.json({
+            message: "Deleted successfully"
+        });
+    })
+});
+
+app.listen(PORT);
