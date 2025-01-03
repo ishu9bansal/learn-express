19d34916734ded41fd9c805b53df8068c330005e
Ishu Bansal <ishubansal1400@gmail.com>
2025-01-03 06:22:38 +0530

    fix student population

--- routes/students.js
+++ routes/students.js
@@ -5,7 +5,7 @@ const Profile = require('../models/profile');
 
 router.get("/", async (req, res) => {
     try {
-        const students = await Student.find().populate('profile.nsted.field grades');
+        const students = await Student.find().populate('profile');
         res.json(students);
     } catch (err) {
         res.status(500).json({ message: err.message });
